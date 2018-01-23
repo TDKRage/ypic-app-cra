19e3a39d8ff501ac181ee8c6697da2618c485baa
Lyle Classen <tdkrage@gmail.com>
2018-01-23 21:00:41 +0200

    added react-router & react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Link
+} from 'react-router-dom'
+import Home from './components/pages/home';
+import Login from './components/pages/login';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+            
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+            <ul>
+              <li><Link to="/">Home</Link> </li>
+              <li><Link to="/login">Login</Link> </li>
+            </ul>
+          </p>
+          <Route path="/" component={Home} exact/>
+          <Route path="/login" component={Login} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
