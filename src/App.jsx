33418acd66a8a074a662ff0c445994c298baea68
Lyle Classen <tdkrage@gmail.com>
2018-01-23 21:54:35 +0200

    react-toobox, toolbox-themr

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,22 @@ import {
 } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+import theme from './assets/react-toolbox/theme';
 import store, { history } from './reducers/store';
 
 import Main from './components/main';
 
+import './assets/react-toolbox/theme.css';
 import './App.css';
 
 const App = () => (
   <BrowserRouter>
     <Provider store={store}>
       <ConnectedRouter history={history}>
-        <Main />
+        <ThemeProvider theme={theme}>
+          <Main />
+        </ThemeProvider>
       </ConnectedRouter>
     </Provider>
   </BrowserRouter>
--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -3,6 +3,8 @@ import {
   Route,
   Link,
 } from 'react-router-dom';
+import Button from 'react-toolbox/lib/button/Button';
+
 import Home from '../pages/home';
 import Login from '../pages/login';
 import logo from '../../logo.svg';
@@ -16,11 +18,19 @@ const Main = () => (
     </header>
     <p className="App-intro">
       To get started, edit <code>src/App.js</code> and save to reload.
-      <ul>
-        <li><Link to="/">Home</Link> </li>
-        <li><Link to="/login">Login</Link> </li>
-      </ul>
     </p>
+    <ul>
+      <li>
+        <Link to="/">
+          <Button>Home</Button>
+        </Link>
+      </li>
+      <li>
+        <Link to="/login">
+          <Button>Login</Button>
+        </Link>
+      </li>
+    </ul>
     <Route path="/" component={Home} exact />
     <Route path="/login" component={Login} />
   </div>
