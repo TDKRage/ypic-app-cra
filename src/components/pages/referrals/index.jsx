9ffdd248756d0d8e861fda71bb62d0f484467269
Unknown <tdkrage@gmail.com>
2018-02-15 21:32:53 +0200

    set to prod and fliped referals

--- src/components/pages/referrals/index.jsx
+++ src/components/pages/referrals/index.jsx
@@ -27,32 +27,32 @@ const Referrals = () => (
     <Card className="referrals-card">
       <CardTitle
         avatar={<Avatar title="South African Depression and Anxiety Group" cover image={SADAGImg} />}
-        title="South African Depression and Anxiety Group"
-        subtitle="Suicide"
+        subtitle="South African Depression and Anxiety Group"
+        title="Suicide"
       />
       <CardText className="referrals-card-text"><a href="tel:0800567567">0800 567 567</a> / <a href="tel:31393">31393 (free sms)</a></CardText>
     </Card>
     <Card className="referrals-card">
       <CardTitle
         avatar={<Avatar title="South African Depression and Anxiety Group" cover image={SADAGImg} />}
-        title="South African Depression and Anxiety Group"
-        subtitle="Substance Abuse"
+        subtitle="South African Depression and Anxiety Group"
+        title="Substance Abuse"
       />
       <CardText className="referrals-card-text"><a href="tel:0800121314">0800 12 13 14</a></CardText>
     </Card>
     <Card className="referrals-card">
       <CardTitle
         avatar={<Avatar title="South African Depression and Anxiety Group" cover image={SADAGImg} />}
-        title="South African Depression and Anxiety Group"
-        subtitle="Cyber Bullying"
+        subtitle="South African Depression and Anxiety Group"
+        title="Cyber Bullying"
       />
       <CardText className="referrals-card-text"><a href="tel:0800212223">0800 21 22 23 (Dr Reddy)</a></CardText>
     </Card>
     <Card className="referrals-card">
       <CardTitle
         avatar={<Avatar title="Tears Foundation" cover image={TearsImg} />}
-        title="Tears Foundation"
-        subtitle="Sexual Abuse / Rape Crisis"
+        subtitle="Tears Foundation"
+        title="Sexual Abuse / Rape Crisis"
       />
       <CardText className="referrals-card-text"><a href="tel:0105905920">010 590 5920</a> / <a href="tel:*134*7355">*134*7355 (free)</a></CardText>
     </Card>
@@ -66,24 +66,24 @@ const Referrals = () => (
     <Card className="referrals-card">
       <CardTitle
         avatar={<Avatar title="Lifeline" cover image={LifelineImg} />}
-        title="Lifeline"
-        subtitle="Emotional well-being ( Trauma, violence, relationship issues etc) - National Counselling"
+        title="Emotional well-being"
+        subtitle="Lifeline - Trauma, violence, relationship issues etc - National Counselling"
       />
       <CardText className="referrals-card-text"><a href="tel:0861322322">0861 322 322</a></CardText>
     </Card>
     <Card className="referrals-card">
       <CardTitle
         avatar={<Avatar title="South African Responsible Gambling Foundation" cover image={GamblingImg} />}
-        title="South African Responsible Gambling Foundation"
-        subtitle="Gambling"
+        subtitle="South African Responsible Gambling Foundation"
+        title="Gambling"
       />
       <CardText className="referrals-card-text"><a href="tel:0800212223">0800 21 22 23 (Dr Reddy)</a></CardText>
     </Card>
     <Card className="referrals-card">
       <CardTitle
         avatar={<Avatar title="Al-Anon and Nar-Anon" cover image={AnonImg} />}
-        title="Al-Anon and Nar-Anon"
-        subtitle="Support groups for families of substance abuse users"
+        subtitle="Al-Anon and Nar-Anon"
+        title="Support groups for families of substance abuse users"
       />
       <CardText className="referrals-card-text"><a href="tel:0861252666">0861 2526 66 (alcoholic)</a> / <a href="tel:0881296791">088 129 6791 (drugs)</a></CardText>
     </Card>
